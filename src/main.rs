1807e1c87379f440604d7901e4938ff9985fd2a9
lterry <randoom97@live.com>
2024-03-24 10:32:49 -0600

    ping handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+    for stream_result in listener.incoming() {
+        match stream_result {
+            Ok(mut stream) => {
+                stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
