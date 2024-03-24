bf214b96f67654f09adcd9b5100c2f0967d3c8ba
lterry <randoom97@live.com>
2024-03-24 10:58:30 -0600

    stream loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn stream_handler(stream: &mut TcpStream) {
+    loop {
+        let mut input_buffer = [0; 1024];
+        let read_result = stream.read(&mut input_buffer);
+        if read_result.is_err() {
+            break;
+        }
+
+        let write_result = stream.write(b"+PONG\r\n");
+        if write_result.is_err() {
+            break;
+        }
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -6,7 +24,7 @@ fn main() {
     for stream_result in listener.incoming() {
         match stream_result {
             Ok(mut stream) => {
-                stream.write(b"+PONG\r\n").unwrap();
+                stream_handler(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
