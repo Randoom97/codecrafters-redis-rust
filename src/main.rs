d31959953d9d4418ecf154350043d81b6db21434
lterry <randoom97@live.com>
2024-03-26 13:14:22 -0600

    better shared state

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,19 @@ use std::{
     env,
     io::Write,
     net::{TcpListener, TcpStream},
-    sync::RwLock,
+    sync::{Arc, RwLock},
     thread,
     time::{Duration, SystemTime},
 };
 
 use parser::{decode, encode_bulk_string, encode_simple_error, encode_simple_string, RedisType};
+
 #[derive(Debug)]
 struct Data {
     value: String,
     expire_time: Option<SystemTime>,
 }
 
-static mut DATA_STORE: Option<RwLock<HashMap<String, Data>>> = None;
 
 fn send(stream: &mut impl Write, message: String) {
     stream.write(message.as_bytes()).unwrap();
@@ -43,7 +43,10 @@ fn get_u64_argument(token: &str, arguments: &Vec<String>) -> Option<u64> {
     return str::parse::<u64>(string_option.unwrap()).ok();
 }
 
-fn stream_handler(mut stream: TcpStream) {
+fn stream_handler(
+    mut stream: TcpStream,
+    data_store: Arc<RwLock<HashMap<String, Data>>>,
+) {
     loop {
         let input_option = decode(&mut stream);
         if input_option.is_none() {
@@ -75,8 +78,8 @@ fn stream_handler(mut stream: TcpStream) {
                             .unwrap(),
                     );
                 }
-                unsafe {
-                    let mut map = DATA_STORE.as_ref().unwrap().write().unwrap();
+
+                let mut map = data_store.write().unwrap();
                     map.insert(
                         key.to_owned(),
                         Data {
@@ -84,15 +87,16 @@ fn stream_handler(mut stream: TcpStream) {
                             expire_time,
                         },
                     );
-                }
+                drop(map);
+
                 send(&mut stream, encode_simple_string("OK"));
             }
             "get" => {
                 let key = &arguments[1];
                 let mut response: Option<String> = None;
                 let mut expired = false;
-                unsafe {
-                    let map = DATA_STORE.as_ref().unwrap().read().unwrap();
+
+                let map = data_store.read().unwrap();
                     let data_option = map.get(key);
                     if data_option.is_some() {
                         let data = data_option.unwrap();
@@ -105,7 +109,8 @@ fn stream_handler(mut stream: TcpStream) {
                             response = Some(encode_bulk_string(Some(&data.value)));
                         }
                     }
-                }
+                drop(map);
+
                 if response.is_none() {
                     send(&mut stream, encode_bulk_string(None));
                 } else {
@@ -114,21 +119,16 @@ fn stream_handler(mut stream: TcpStream) {
 
                 // cleanup if we found the value expired
                 if expired {
-                    unsafe {
                         // make sure it didn't get updated in the short time the lock was released before removing
-                        let mut map = DATA_STORE.as_ref().unwrap().write().unwrap();
+                    let mut map = data_store.write().unwrap();
                         let data_option = map.get(key);
                         if data_option.is_some()
                             && data_option.unwrap().expire_time.is_some()
-                            && SystemTime::now().gt(data_option
-                                .unwrap()
-                                .expire_time
-                                .as_ref()
-                                .unwrap())
+                        && SystemTime::now().gt(data_option.unwrap().expire_time.as_ref().unwrap())
                         {
                             map.remove(key);
-                        }
                     }
+                    drop(map);
                 }
             }
             "echo" => {
@@ -151,16 +151,16 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let port = get_u64_argument("--port", &args);
 
-    unsafe {
-        DATA_STORE = Some(RwLock::new(HashMap::new()));
-    }
+    let mut data_store: Arc<RwLock<HashMap<String, Data>>> = Arc::new(RwLock::new(HashMap::new()));
+
     let listener =
         TcpListener::bind("127.0.0.1:".to_owned() + &port.unwrap_or(6379).to_string()).unwrap();
 
     for stream_result in listener.incoming() {
         match stream_result {
             Ok(stream) => {
-                thread::spawn(|| stream_handler(stream));
+                let data_store = Arc::clone(&mut data_store);
+                thread::spawn(move || stream_handler(stream, data_store));
             }
             Err(e) => {
                 println!("error: {}", e);
